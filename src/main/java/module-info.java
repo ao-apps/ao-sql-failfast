a23b1466aef084aaa542e15781fb475737f6e959
AO Industries, Inc <support@aoindustries.com>
2021-06-29 02:55:46 -0500

    Now supports Java 9+ modules.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,32 @@
+/*
+ * ao-sql-failfast - Fail-fast JDBC wrapper.
+ * Copyright (C) 2021  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-sql-failfast.
+ *
+ * ao-sql-failfast is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-sql-failfast is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-sql-failfast.  If not, see <http://www.gnu.org/licenses/>.
+ */
+module com.aoapps.sql.failfast {
+	exports com.aoapps.sql.failfast;
+	provides java.sql.Driver with com.aoapps.sql.failfast.Driver;
+	// Direct
+	requires com.aoapps.collections; // <groupId>com.aoapps</groupId><artifactId>ao-collections</artifactId>
+	requires com.aoapps.lang; // <groupId>com.aoapps</groupId><artifactId>ao-lang</artifactId>
+	requires com.aoapps.sql.wrapper; // <groupId>com.aoapps</groupId><artifactId>ao-sql-wrapper</artifactId>
+	// Java SE
+	requires java.sql;
+}
